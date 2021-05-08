a001248408716d30d5aabc80c1ba4c7df5bc756d
Jk348 <82089102+Jk348@users.noreply.github.com>
2021-05-08 19:01:08 +0530

    Add files via upload

---
+++ python-mysql/db/user.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE roytuts;
+use roytuts;
+
+CREATE TABLE `user` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` int unsigned NOT NULL,
+  `address` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+insert  into `user`(`id`,`name`,`email`,`phone`,`address`) values
+(1,'Ravi','ravi52@gmail.com',2147483647,'India'),
+(2,'mark','mark27@gmail.com',34256780,'England');
